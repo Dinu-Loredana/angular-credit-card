baf7646ea9ab1f29229f0deac9d214d815bba05c
Dinu-Loredana <dinuloredana.m@gmail.com>
2022-08-31 16:48:31 +0100

    add styling on validation messages

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -9,7 +9,14 @@ import { FormControl } from '@angular/forms';
 export class InputComponent implements OnInit {
   @Input() control!: FormControl; // declaring without passing initial val; use ! to avoid TS err
   // use Input decorator to receive data from the parent via 'control' property
+  // use Input decorator to receive data from the parent via 'control' property
+  @Input() label!: string;
   constructor() {}
 
   ngOnInit(): void {}
+
+  showErrors() {
+    const { dirty, touched, errors } = this.control;
+    return dirty && touched && errors;
+  }
 }
